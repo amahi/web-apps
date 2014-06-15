8c852157d954d35d760f568cd520092f3af03192
Arpit Goyal <agmps17@gmail.com>
2014-06-15 11:38:50 +0530

    Changes as per the feedback
    Signed-off-by: Arpit Goyal <agmps17@gmail.com>

--- lib/webapps/engine.rb
+++ lib/webapps/engine.rb
@@ -4,5 +4,8 @@ module Webapps
 		# want to adjust all your controllers views, etc., making Amahi's
 		# platform hard to reach from here
 		# isolate_namespace Webapps
+		initializer :assets do |config|
+	      Rails.application.config.assets.paths << root.join("app", "assets", "images")
+	    end
 	end
 end
