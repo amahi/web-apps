1bc8796b5f8bf40c6e9bf89f0d9742b6d84400a5
Arpit Goyal <agmps17@gmail.com>
2014-07-08 22:29:55 +0530

    changes to make bootstrap compatible with webapps plugin

--- lib/webapps/engine.rb
+++ lib/webapps/engine.rb
@@ -7,12 +7,6 @@ module Webapps
 		initializer :assets do |config|
 	      Rails.application.config.assets.paths << root.join("app", "assets", "images")
 	    end
-	    initializer :append_migrations do |app|
- 			unless app.root.to_s.match root.to_s
- 				config.paths["db/migrate"].expanded.each do |expanded_path|
- 					app.config.paths["db/migrate"] << expanded_path
- 				end
- 			end
- 		end
+
 	end
 end
